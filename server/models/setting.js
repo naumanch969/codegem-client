c323291a12fedd2d8a5b3a81f0f4cbb86597db29
naumanch969 <naumanch969@gmail.com>
2024-04-16 23:24:21 +0500

    settings model added

--- server/models/setting.js
+++ server/models/setting.js
@@ -1,9 +1,49 @@
 import { Schema, model } from "mongoose";
 
-const settingSchema = Schema({
-  
-}, { timestamps: true });
+const settingsSchema = new Schema(
+  {
+    accountSettings: {
+      username: String,
+      fullName: String,
+      email: String,
+      location: String,
+      bio: String,
+    },
+    privacySettings: {
+      manageVisibilityOfProfile: String,
+      controlWhoCanSendFriendRequests: String,
+      controlWhoCanSeeMyFriendsList: String,
+      controlWhoCanSeeMyPosts: String,
+      controlWhoCanTagMeInPosts: String,
+      blockUsers: Number,
+      manageAppPermissions: Number,
+    },
+    notificationSettings: {
+      emailNotifications: String,
+      pushNotifications: String,
+      manageNotificationPreferences: Number,
+    },
+    themeAndDisplaySettings: {
+      chooseDarkLightTheme: String,
+      adjustFontSize: String,
+    },
+    connectedAccounts: {
+      facebook: String,
+      twitter: String,
+      github: String,
+    },
+    accessibilitySettings: {
+      enableAccessibilityFeatures: String,
+    },
+    helpAndSupport: {
+      contactSupport: String,
+      helpCenter: String,
+    },
+    logOut: String, 
+  },
+  { timestamps: true } // Adding timestamps option
+);
 
-const settingModel = model('Setting', settingSchema);
+const settingModel = model("Setting", settingSchema);
 
-export default settingModel
+export default settingModel;
